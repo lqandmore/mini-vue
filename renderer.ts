a5f7c30d170df6ae35dc805a4da211ad7d8ba9ed
liquan <lqandmore@163.com>
2023-02-16 11:53:25 +0800

    渲染器

---
+++ renderer.ts
@@ -0,0 +1,50 @@
+interface vNode {
+  tag: string,
+  props: any ,
+  children: any,
+  el: Element | null
+}
+
+type hFn = (tag: string, props: any ,children: any) => vNode
+
+type mountFn = (n: vNode, container: Element)=> void
+
+type patchFn = (n1: vNode, n2: vNode)=> void
+
+const h: hFn = (tag, props,children, el = null) => {
+  return {
+    tag,
+    props,
+    children,
+    el
+  }
+}
+
+const mount: mountFn = (n, container)=> {
+  //根据虚拟node，创建出真实的元素
+  const el = n.el = document.createElement(n.tag)
+  //添加props
+  if (n.props) {
+    for (const key of n.props) {
+      const value = n.props[key]
+      if(key.startsWith('on')) {
+        el.addEventListener(key.slice(2).toLowerCase(),value)
+      }else {
+        el.setAttribute(key, value)
+      }
+    }
+  }
+  //处理children
+  if (n.children) {
+    if (typeof n.children === 'string') {
+      el.textContent = n.children
+    } else {
+      n.children.array.forEach(element => {
+        mount(element, el)
+      });
+    }
+  }
+  //挂载el
+  container.appendChild(el)
+
+}
